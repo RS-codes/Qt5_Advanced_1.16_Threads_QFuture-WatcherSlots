a7271de4d799712effcb5d135c4e6aea06dce77c
RS-codes <RS-codes@github.com>
2021-07-26 10:28:47 +0530

    SourceCode added

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include "test.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Test test;
+    test.process();
+
+    return a.exec();
+}
